3cb4fc31a915eb6ec9ff2609e67f1b83c36e7a9e
Apache_Garuda3169 <sultanmuradsafarov@gmail.com>
2024-07-10 13:48:26 +0400

    created add to favorites card in highlights section

--- common/src/main/java/me/safarov399/common/custom_views/highlights/HighlightsAddFavoritesCard.kt
+++ common/src/main/java/me/safarov399/common/custom_views/highlights/HighlightsAddFavoritesCard.kt
@@ -1,4 +1,15 @@
 package me.safarov399.common.custom_views.highlights
 
-class HighlightsAddFavoritesCard {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import me.safarov399.common.databinding.HighlightsAddFavoritesCardViewBinding
+
+class HighlightsAddFavoritesCard @JvmOverloads constructor(
+    context: Context,
+    attr: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attr, defStyleAttr) {
+    private val binding = HighlightsAddFavoritesCardViewBinding.inflate(LayoutInflater.from(context), this, true)
 }
